a905f452617175bcf5eff35c4854273dcd6ce1a0
eva <eva@DESKTOP-G4RCT69>
2021-08-29 18:13:40 -0500

    Agregar archivos de proyecto.

---
+++ AppConsolaHolaMundoCs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsolaHolaMundoCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            //Sumar
+            int a = 10;
+            int b = 16;
+            int c = a + b;
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
